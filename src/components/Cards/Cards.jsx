ac116d95f6da3adaa0be99053b5c224480fb1ca9
Migs Munar <miguel.munar@flexisourceit.com.au>
2020-04-21 22:34:05 +0800

    Components boilerplate

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+
+import styles from './Cards.module.css';
+
+export default function Cards(props) {
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                
+            </Grid>
+        </div>
+    );
+}
